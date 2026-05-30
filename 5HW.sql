37e7ca95a66a4389f00950311c56ec7f6b3ff789
dsbarabash <dary.barabash@yandex.ru>
2026-05-30 15:46:09 +0300

    Update 5HW.sql

--- 5HW.sql
+++ 5HW.sql
@@ -1,4 +1,4 @@
---Напишите запрос, который выводит название фильма и список языков, на которых доступен фильм. 
+-- Напишите запрос, который выводит название фильма и список языков, на которых доступен фильм. 
 -- Используйте функции работы с JSON для извлечения массива языков из поля additional_info.
 
 SELECT title, additional_info->'languages' FROM movies  
@@ -10,7 +10,7 @@ SELECT title, additional_info->'languages' FROM movies
 SELECT title, additional_info->'budget' FROM movies WHERE (additional_info->'budget')::INT > 100000000
 
 
---Напишите запрос, который для каждого клиента создаёт JSON-объект с полями full_name
+-- Напишите запрос, который для каждого клиента создаёт JSON-объект с полями full_name
 -- (содержащим полное имя клиента) и contact (содержащим email и номер телефона). 
 -- Выведите customer_id и созданный JSON-объект.
 
@@ -21,7 +21,7 @@ SELECT json_build_object(
 FROM customers
 
 
---Напишите запрос, который добавляет новый предпочитаемый жанр ""Drama"" в список 
+-- Напишите запрос, который добавляет новый предпочитаемый жанр ""Drama"" в список 
 -- preferred_genres для всех клиентов, которые подписаны на рассылку новостей
 -- (ключ newsletter имеет значение true).
 
@@ -35,3 +35,27 @@ WHERE (preferences->'newsletter')::BOOL = true
 -- что жанр хранится в поле genre таблицы Movie, а бюджет — внутри JSON-поля additional_info.
 
 SELECT genre, AVG((additional_info->'budget')::INT) from movies GROUP BY genre
+
+
+-- Задание со звездочкой - выполняется по желанию.
+-- 6. Напишите запрос, который выводит список клиентов, у которых в preferences указан предпочитаемый актёр ""Leonardo DiCaprio"".
+
+SELECT *  FROM customers  WHERE preferences->'preferred_actors' @> '["Leonardo DiCaprio"]'::JSONB
+
+
+-- 7. Напишите запрос, который выводит список фильмов, отсортированных по значению кассовых сборов box_office из поля additional_info в порядке убывания.
+
+SELECT title, additional_info->'box_office' from movies order by additional_info->'box_office' DESC
+
+-- 8. Напишите запрос, который выводит название фильма, его жанр и количество наград (awards) из additional_info.
+
+SELECT title, genre, additional_info->'awards' as awards from movies 
+
+-- 9. Напишите запрос, который подсчитывает количество фильмов, имеющих более чем одну награду в поле awards внутри additional_info.
+
+
+
+-- 10. Напишите запрос, который удаляет ключ preferred_actors из поля preferences для всех клиентов. 
+
+
+
