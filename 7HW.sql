98293e43829b61ec871f7eab187fe1363dd2d747
dsbarabash <dary.barabash@yandex.ru>
2026-06-27 14:30:21 +0300

    Update 7HW.sql

--- 7HW.sql
+++ 7HW.sql
@@ -35,23 +35,23 @@
 
         --Создайте триггер, который предотвращает удаление записей о фильмах, если они связаны с таблицей Rental. Используйте BEFORE DELETE триггер.
 
-	create or replace function keep_rented_films()
-	returns trigger
-	language plpgsql
-	as $$
-	begin
-	 select count(*) as rents from rentals where movie_id=old.movie_id;
-	  if (rents > 0) then
-	    RAISE EXCEPTION 'Запрещено удаление фильма котоырй ранее был арендован';
-	  end if;
-
-	return null;
-	end;
-	$$;
+		create or replace function keep_rented_films()
+		returns trigger
+		language plpgsql
+		as $$
+		begin
+		 select count(*) as rents from rentals where movie_id=old.movie_id;
+		  if (rents > 0) then
+		    RAISE EXCEPTION 'Запрещено удаление фильма котоырй ранее был арендован';
+		  end if;
 	
-	CREATE TRIGGER emp_audit
-	BEFORE DELETE ON movies
-	    FOR EACH ROW EXECUTE function keep_rented_films();
+		return null;
+		end;
+		$$;
+		
+		CREATE TRIGGER emp_audit
+		BEFORE DELETE ON movies
+		    FOR EACH ROW EXECUTE function keep_rented_films();
 
 
 
@@ -61,23 +61,23 @@
     -- Последовательности (Sequences)
     --     Создайте последовательность actor_sequence, которая будет генерировать уникальные значения для новых актеров. Начальное значение должно быть 1000, шаг увеличения — 1.
 
-CREATE SEQUENCE actor_sequence
-AS bigint
-START WITH 1000
-INCREMENT BY 1
-NO CYCLE
-OWNED BY actors.actor_id
+	CREATE SEQUENCE actor_sequence
+	AS bigint
+	START WITH 1000
+	INCREMENT BY 1
+	NO CYCLE
+	OWNED BY actors.actor_id
 
 	
     --     Добавьте нового актера в таблицу Actor, используя значение из созданной последовательности для поля actor_id.
 
-insert into actors values (nextval('actor_sequence'), 'Tom', 'Holand', '1996-06-01', 'British');
+	insert into actors values (nextval('actor_sequence'), 'Tom', 'Holand', '1996-06-01', 'British');
 
 
     --     Обновите последовательность, чтобы начальное значение было на 10 больше последнего созданного значения. Проверьте изменение.
-
-select setval('actor_sequence', currval('actor_sequence')+10, true);
-select currval('actor_sequence');
+	
+	select setval('actor_sequence', currval('actor_sequence')+10, true);
+	select currval('actor_sequence');
 
 
     -- Триггеры (Triggers)
